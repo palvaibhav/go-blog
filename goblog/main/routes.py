716e859e74ff4ab66b65f6d9a1d744e90a271844
palvaibhav <vaibhav171997@gmail.com>
2019-08-07 17:57:09 +0530

    Added templates and database

--- goblog/main/routes.py
+++ goblog/main/routes.py
@@ -1,8 +1,13 @@
-from flask import Blueprint
+from flask import render_template, request, Blueprint
+from goblog.models import Post
 
 main = Blueprint('main', __name__)
+
 
 @main.route('/')
 @main.route('/home')
 def home():
-    return "This page will show all the blog posts"
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(
+        Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template('home.html', posts=posts)
